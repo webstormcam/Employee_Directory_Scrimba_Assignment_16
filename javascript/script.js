76e8005006a2da0ff28b29e3af17f6db6ba0f0bc
Cameron Chandler <webstormcam@gmail.com>
2023-03-30 22:12:46 -0400

    Making things render

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,38 @@
 import employees from "./employees.js";
-
+const gridArea = document.getElementById('gridArea')
 let rolePicked = document.getElementById('roleFinder')
 
 const allEmployeesRender = () =>{
-    console.clear()
+    gridArea.innerHTML=``
+    let people =``
     for(const person of employees  )
     if(rolePicked.value===`everyone`){
-        console.log(person)
+        people+=`<div class="grid-item">
+        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+        <div class="name-title">
+            <h2>${person.name}</h2>
+            <h3>${person.title}</h3>
+        </div>
+        ${person.bio}
+            <div class="socials">
+              <a href="https://twitter.com/"><img src="./images/twitter 2.png" alt=""></a>
+              <a href="https://www.linkedin.com/feed/"> <img src="./images/linkedin 1.png" alt=""></a>
+            </div>`
     } else if(person.team===rolePicked.value){
-        console.log(person)
+        people+=`<div class="grid-item">
+        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+        <div class="name-title">
+            <h2>${person.name}</h2>
+            <h3>${person.title}</h3>
+        </div>
+        ${person.bio}
+            <div class="socials">
+              <a href="https://twitter.com/"><img src="./images/twitter 2.png" alt=""></a>
+              <a href="https://www.linkedin.com/feed/"> <img src="./images/linkedin 1.png" alt=""></a>
+            </div>
+`
     }
-    
+    gridArea.innerHTML=people
 }
 rolePicked.addEventListener('change',allEmployeesRender)
 allEmployeesRender()
