3bab67b6d901ff3e6e9d2021b6f0920a65afca98
Cameron Chandler <webstormcam@gmail.com>
2023-03-31 23:31:53 -0400

    Got the socials to render dynamically

--- javascript/script.js
+++ javascript/script.js
@@ -2,34 +2,45 @@ import employees from "./employees.js";
 const gridArea = document.getElementById('gridArea')
 let rolePicked = document.getElementById('roleFinder')
 
+
+
 const allEmployeesRender = () =>{
     gridArea.innerHTML=``
     let people =``
-    let socialCollecter =``
-    for(const person of employees  )
-    if(rolePicked.value===`everyone`){
-        people+=`<div class="grid-item">
-        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
-        <div class="name-title">
-            <h2>${person.name}</h2>
-            <h3>${person.title}</h3>
-        </div>
-        ${person.bio}
-            <div class="socials">
+    for(const person of employees  ){
+        let socialCollecter =``
+        if(person.social.twitter){
+           socialCollecter+=`<a href="${person.social.twitter}"><img src="./images/twitter 2.png"></a>`
+        }
+        if(person.social.linkedin){
+            socialCollecter+=`<a href="${person.social.linkedin}"><img src="./images/linkedin 1.png"></a>`
+        }
+        if(rolePicked.value===`everyone`){
+            people+=`<div class="grid-item">
+            <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+            <div class="name-title">
+                <h2>${person.name}</h2>
+                <h3>${person.title}</h3>
             </div>
-            </div>`
-    } else if(person.team===rolePicked.value){
-        people+=`<div class="grid-item">
-        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
-        <div class="name-title">
-            <h2>${person.name}</h2>
-            <h3>${person.title}</h3>
-        </div>
-        ${person.bio}
-            <div class="socials">
+            ${person.bio}
+                <div class="socials">
+                ${socialCollecter}
+                </div>
+                </div>`
+        } else if(person.team===rolePicked.value){
+            people+=`<div class="grid-item">
+            <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+            <div class="name-title">
+                <h2>${person.name}</h2>
+                <h3>${person.title}</h3>
             </div>
-            </div>
-`
+            ${person.bio}
+                <div class="socials">
+                ${socialCollecter}
+                </div>
+                </div>
+    `
+        }
     }
     gridArea.innerHTML=people
 }
