dc2e2ed2f4c5891be5fd231de83e16581dda787b
Cameron Chandler <webstormcam@gmail.com>
2023-04-01 11:45:42 -0400

    basically done now

--- javascript/script.js
+++ javascript/script.js
@@ -47,12 +47,42 @@ const allEmployeesRender = () =>{
 }
 rolePicked.addEventListener('change',allEmployeesRender)
 NameSearch.addEventListener('keyup',function(){
-    console.clear()
+    let people =``
 for(const person of employees){
-    if(person.name.toLowerCase().includes(NameSearch.value)){
-        console.log(person)
+    let socialCollecter =``
+        if(person.social.twitter){
+           socialCollecter+=`<a href="${person.social.twitter}"><img src="./images/twitter 2.png"></a>`
+        }
+        if(person.social.linkedin){
+            socialCollecter+=`<a href="${person.social.linkedin}"><img src="./images/linkedin 1.png"></a>`
+        }
+    if(person.name.toLowerCase().includes(NameSearch.value)&& rolePicked.value==="everyone"){
+        people+=`<div class="grid-item">
+        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+        <div class="name-title">
+            <h2>${person.name}</h2>
+            <h3>${person.title}</h3>
+        </div>
+        ${person.bio}
+            <div class="socials">
+            ${socialCollecter}
+            </div>
+            </div>`
+    } else if(person.name.toLowerCase().includes(NameSearch.value)&& person.team===rolePicked.value){
+        people+=`<div class="grid-item">
+        <img class="profile-pic" src="./images/${person.image}" alt="A man with glasses">
+        <div class="name-title">
+            <h2>${person.name}</h2>
+            <h3>${person.title}</h3>
+        </div>
+        ${person.bio}
+            <div class="socials">
+            ${socialCollecter}
+            </div>
+            </div>`
     }
 }
+gridArea.innerHTML=people
   
    
 })
